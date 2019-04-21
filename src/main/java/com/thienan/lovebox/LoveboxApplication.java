d9eaf915abdf0b3e660055316845d754b4af641c
Nguyen Phuc Thien An <npta97@gmail.com>
2019-04-21 10:48:36 +0700

    Init project

---
+++ src/main/java/com/thienan/lovebox/LoveboxApplication.java
@@ -0,0 +1,13 @@
+package com.thienan.lovebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thienan/lovebox/LoveboxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thienan.lovebox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoveboxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
